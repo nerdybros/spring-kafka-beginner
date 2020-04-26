5b909268d83ef69f8bd0ec4755e370caefb621d6
Junhyunny <kang3966@naver.com>
2020-04-27 01:29:48 +0900

    2020-04-27 update

--- producer/src/main/java/io/nerdybros/springkafkabeginner/producer/rest/ProducerController.java
+++ producer/src/main/java/io/nerdybros/springkafkabeginner/producer/rest/ProducerController.java
@@ -1,62 +1,67 @@
 package io.nerdybros.springkafkabeginner.producer.rest;
 
-import io.nerdybros.springkafkabeginner.producer.SimpleProducer;
-import io.nerdybros.springkafkabeginner.producer.SimpleProducerCallback;
-import io.nerdybros.springkafkabeginner.producer.SimplePartitionProducer;
-import io.nerdybros.springkafkabeginner.producer.SimpleTransactionalProducer;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import io.nerdybros.springkafkabeginner.producer.SimplePartitionProducer;
+import io.nerdybros.springkafkabeginner.producer.SimpleProducer;
+import io.nerdybros.springkafkabeginner.producer.SimpleProducerCallback;
+import io.nerdybros.springkafkabeginner.producer.SimpleTransactionalProducer;
 
 @RestController
 @RequestMapping("producer")
 public class ProducerController {
 
-    @Autowired
-    @Qualifier("simpleProducer")
-    private SimpleProducer producer;
+	@Autowired
+	@Qualifier("simpleProducer")
+	private SimpleProducer producer;
 
-    @Autowired
-    @Qualifier("simpleProducerCallback")
-    private SimpleProducerCallback producerCallback;
+	@Autowired
+	@Qualifier("simpleProducerCallback")
+	private SimpleProducerCallback producerCallback;
 
-    @Autowired
-    @Qualifier("simplePartitionProducer")
-    private SimplePartitionProducer partitionProducer;
+	@Autowired
+	@Qualifier("simplePartitionProducer")
+	private SimplePartitionProducer partitionProducer;
 
-    @Autowired
-    @Qualifier("simpleTransactionalProducer")
-    private SimpleTransactionalProducer transactionalProducer;
+	@Autowired
+	@Qualifier("simpleTransactionalProducer")
+	private SimpleTransactionalProducer transactionalProducer;
 
-    @PostMapping("/simple")
-    public void sendUsingSimpleProducer(@RequestBody String message) {
-        this.producer.sendMessage(message);
-    }
+	@PostMapping("/simple")
+	public void sendUsingSimpleProducer(@RequestBody String message) {
+		this.producer.sendMessage(message);
+	}
 
-    @PostMapping("/simple-callback")
-    public void sendUsingSimpleProducerCallback(@RequestBody String message) {
-        this.producerCallback.sendMessage(message);
-    }
+	@PostMapping("/simple-callback")
+	public void sendUsingSimpleProducerCallback(@RequestBody String message) {
+		this.producerCallback.sendMessage(message);
+	}
 
-    @PostMapping("/simple-partition")
-    public void sendUsingSimplePartitionProducer(@RequestParam String key, @RequestBody String message) {
-        this.partitionProducer.sendMessage(key, message);
-    }
+	@PostMapping("/simple-partition")
+	public void sendUsingSimplePartitionProducer(@RequestParam String key, @RequestBody String message) {
+		this.partitionProducer.sendMessage(key, message);
+	}
 
-    @PostMapping("/simple-partition-no-key")
-    public void sendUsingSimplePartitionNoKey(@RequestBody String message) {
-        this.partitionProducer.sendMessageNoMessageKey(message);
-    }
+	@PostMapping("/simple-partition-no-key")
+	public void sendUsingSimplePartitionNoKey(@RequestBody String message) {
+		this.partitionProducer.sendMessageNoMessageKey(message);
+	}
 
-    @PostMapping("/simple-transactional-method")
-    public void sendUsingSimpleTransactionalProducerMethod(@RequestBody List<String> messages) {
-        this.transactionalProducer.sendMessageUsingMethod(messages);
-    }
+	@PostMapping("/simple-transactional-method")
+	public void sendUsingSimpleTransactionalProducerMethod(@RequestBody List<String> messages) {
+		this.transactionalProducer.sendMessageUsingMethod(messages);
+	}
 
-    @PostMapping("/simple-transactional-annotation")
-    public void sendUsingSimpleTransactionProducerAnnotation(@RequestBody List<String> messages) {
-        this.transactionalProducer.sendMessageUsingAnnotation(messages);
-    }
+	@PostMapping("/simple-transactional-annotation")
+	public void sendUsingSimpleTransactionProducerAnnotation(@RequestBody List<String> messages) {
+		this.transactionalProducer.sendMessageUsingAnnotation(messages);
+	}
 }
